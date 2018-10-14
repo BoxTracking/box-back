ba8929f2609b459edfe1f1eec45cab80aa525ec9
Maharsh Patel <patel@maharsh.net>
2018-10-14 14:30:54 -0600

    updated welcome string

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Box: Parcel Tracking Application').end();
+  res.status(200).send('!Box: Parcel Tracking Application').end();
 });
 
 module.exports = app
